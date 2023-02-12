ad4193f4171a2c07230ce0e5412b2a51e35c53a4
zhengx55 <williamzhxin@gmail.com>
2023-02-12 22:39:07 +0800

    configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: ["rb.gy"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/components/Header.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import {
+  BellIcon,
+  DocumentMagnifyingGlassIcon,
+} from "@heroicons/react/24/solid";
+import Link from "next/link";
+function Header() {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 0 ? setIsScrolled(true) : setIsScrolled(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll); 
+    };
+  }, []);
+  return (
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          alt="netflix"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">Tv Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <DocumentMagnifyingGlassIcon className="hidden sm:inline h-6 w-6 " />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6 " />
+        <Link href={"/accounts"}>
+          <img
+            src="https://rb.gy/glpwyx"
+            alt="account"
+            className="cursor-pointer rounded-sm"
+          />
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
